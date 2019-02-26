7be33b116ecb0093e5923c2d53d4e952cb2fb0ad
Mario <mariobischoffneto@hotmail.com>
2019-02-26 00:18:56 -0300

    fix on view

--- cliente/app/ui/views/NegociacoesView.js
+++ cliente/app/ui/views/NegociacoesView.js
@@ -22,15 +22,14 @@ class NegociacoesView {
                 </thead>
                 
                 <tbody>
-                ${model.paraArray().map(neogiacao => {
-                    return 
-                        `
-                            <tr>
-                                <td>${DateConverter.paraTexto(negociacao.data)}</td>
-                                <td>${negociacao.quantidade}</td>
-                                <td>${negociacao.valor}</td>
-                                <td>${negociacao.volume}>/td>
-                            </tr>
+                ${model.paraArray().map(negociacao => {
+                    return `
+                        <tr>
+                            <td>${DateConverter.paraTexto(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>
                         `
                 }).join('')}
                 </tbody>
