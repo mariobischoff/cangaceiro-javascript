08983b4af152c56179792ec30f2078923426b06f
Mario <mariobischoffneto@hotmail.com>
2019-02-25 23:35:21 -0300

    add view

--- cliente/app/controllers/NegociacaoController.js
+++ cliente/app/controllers/NegociacaoController.js
@@ -7,24 +7,33 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+        this._negociacoesView.update(this._negociacoes);
 
     }
 
     adiciona(event) {
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
+        this._limpaFormulario();
+    }
 
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
 
-        let negociacao = new Negociacao(
-            DateConverter.paraData(this._inputData.value), 
-            parseInt(this._inputQuantidade.value), 
+    _criaNegociacao() {
+        // retorna uma instancia de Negociação
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        );
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(negociacao);
-        
-        console.log(diaMesAno);
-        
+        )
     }
 
 }
