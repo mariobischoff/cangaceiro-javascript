ce02fdd5d589043d8d1a89e4103a3c0ee13b7a27
Mario <mariobischoffneto@hotmail.com>
2019-02-27 15:58:49 -0300

    bind cap-10

--- cliente/app/controllers/NegociacaoController.js
+++ cliente/app/controllers/NegociacaoController.js
@@ -8,25 +8,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        const self = this;
-        this._negociacoes = new Proxy(new Negociacoes(), {
-            get(target, prop, receiver) {
-                if(typeof(target[prop]) == typeof(Function) && ['adiciona', 'esvazia'].includes(prop)) {
-                    return function() {
-                        target[prop].apply(target, arguments);
-                        self._negociacoesView.update(target);
-                    }
-                } else {
-                    return target[prop];
-                }
-            }
-        });
+        this._negociacoes = new Bind(
+            new Negociacoes(),
+            new NegociacoesView('#negociacoes'),
+            'adiciona', 'esvazia'
+        );
 
-        this._negociacoesView = new NegociacoesView('#negociacoes');
-        this._negociacoesView.update(this._negociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView('#mensagemView');
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView('#mensagemView'),
+            'texto'
+        );
 
     }
 
@@ -34,14 +26,12 @@ class NegociacaoController {
         event.preventDefault();
         this._negociacoes.adiciona(this._criaNegociacao());
         this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
     }
     
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
     }
 
     _limpaFormulario() {
