eb02735736d82c66c0859c48ab7987f36c6eb72e
Mário Bischoff <mariobischoffneto@hotmail.com>
2019-02-28 20:02:08 -0300

    throw, try, catch cap-11

--- cliente/app/ui/converters/DataConverter.js
+++ cliente/app/ui/converters/DataConverter.js
@@ -10,10 +10,12 @@ class DateConverter {
 
     static paraData(texto) {
         // incrementa 1 no mes
-        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto))
-            throw new Error('Deve estar no formato aaaa-mm-dd');
+        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto))
+            throw new DataInvalidaException();
         return new Date(
-            ...texto.split('-').map((item, indice) => item - indice % 2) // arrow function, ES2015 (ES6)
+            ...texto.split('/')
+            .reverse()
+            .map((item, indice) => item - indice % 2) // arrow function, ES2015 (ES6)
         );
     }
 
